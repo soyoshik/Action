b4f8436b35763d5b6c0e41e72ef31c0b0f324c22
soyoshik <deekee4444@gmail.com>
2018-07-26 11:46:08 +0900

    commit

--- src/js/mainNormal.js
+++ src/js/mainNormal.js
@@ -1,86 +1,21 @@
 console.log("mainNormal.js");
 
-
-// var shutterA = document.querySelector('.shutter-a');
-// var shutterB = document.querySelector('.shutter-b');
-//
-// var ShutterAnimation = Barba.BaseTransition.extend({
-//
-//   start: function() {
-//     this.shutter(400)
-//     .then(this.newContainerLoading)
-//     .then(this.finish.bind(this));
-//   },
-//
-//   shutter: function(timer) {
-//     return new Promise(function(resolve){
-//
-//       shutterA.classList.toggle('moved');
-//       shutterB.classList.toggle('moved');
-//
-//       setTimeout(function() {
-//         resolve();
-//       },timer);
-//
-//     });
-//   },
-//
-//   finish:function() {
-//     document.body.scrollTop = 0;
-//     this.done();
-//   }
-//
-// });
-//
-// Barba.Pjax.getTransition = function() {
-//   return ShutterAnimation;
-// };
-//
-// Barba.Pjax.start();
-
-
-var shutterA = document.querySelector('.shutter-a');
-
-var ShutterAnimation = Barba.BaseTransition.extend({
-
-  start: function() {
-    this.shutter(1000)
-    .then(this.newContainerLoading)
-    .then(this.finish.bind(this));
-  },
-
-  shutter: function(timer) {
-    return new Promise(function(resolve){
-      shutterA.classList.toggle('moved');
-      setTimeout(function() {
-        resolve();
-      },timer);
-
-    });
-  },
-
-  finish:function() {
-    shutterA.classList.toggle('moved');
-    document.body.scrollTop = 0;
-    this.done();
-  }
-
-});
-
-Barba.Pjax.getTransition = function() {
-  return ShutterAnimation;
-};
-
-Barba.Pjax.start();
-
-
 //amimejs
-$('#button').on('click', function() {
-  anime({
-    targets: "#testBall",
-    translateX: {
-    value: '+=150',
-    duration: 1000
-  }
-  });
+
+//オープニングアニメーション
+var LandAnim = anime({
+   targets: '.Land__logo path',
+   strokeDashoffset: [anime.setDashoffset, 0],
+   easing: 'easeInOutSine',
+   duration: 1000,
+   delay: function(el, i){ return i* 250 },
+   direction: 'alternate',
+   loop: 'true',
+   //Callback
+   complete: function() {
+      $('.Land__logoSVG path').each(function(e) {
+         $(this).addClass('active');
+         $(this).css('stroke', 'transparent');
+       });
+   }
 });
